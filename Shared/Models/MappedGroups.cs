8d61fe74d147134916946a6e249fc2e1e88125de
Sean Page <sean@torsiontools.com>
2025-01-24 22:21:12 -0600

    Start of Parameter mapping for flexibility of Model Parameters

---
+++ Shared/Models/MappedGroups.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace Torsion.Models
+{
+    internal class MappedGroups
+    {
+        [JsonPropertyName("parameterGroups")]
+        public List<MappedParameterGroup> ParameterGroups { get; set; }
+    }
+}
---
+++ Shared/Models/MappedParameterGroup.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace Torsion.Models
+{
+    internal class MappedParameterGroup
+    {
+        public string Name { get; set; }
+        [JsonPropertyName("parameters")]
+        public List<MappedParameter> Parameters {  get; set; }
+    }
+}
